47b32b12567ae7e0a67cb7aac5b09b50a38768e9
Jon Barlow <jon.barlow@fitbit.com>
2017-08-24 14:26:34 +0100

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# sdk-altimeter
-Fitbit SDK example application.
+# Altimeter
+
+Fitbit SDK example application which simulates an altimeter using the Barometer
+sensor.
+
+Find out more information on the
+[Fitbit Developer Website](https://dev.fitbit.com).
+
+## License
+
+This example is licensed under the [MIT License](./LICENSE).
---
+++ app/index.js
@@ -0,0 +1,28 @@
+import { document } from "document";
+import { Barometer } from "barometer";
+
+console.log("Altimeter starting!");
+
+// Fetch UI elements we will need to change
+var altitudeLabel = document.getElementById("altitude");
+var pressureLabel = document.getElementById("pressure");
+
+// Initialize the UI with some values
+altitudeLabel.innerText = "-";
+
+// Create a new instance of the Barometer
+var bar = new Barometer();
+
+bar.onreading = function() {
+  altitudeLabel.innerText = altitudeFromPressure(bar.pressure / 100) + " ft";
+  pressureLabel.innerText = Math.round(bar.pressure / 100) + " hPa";
+}
+
+// Begin monitoring the sensor
+bar.start();
+
+// Converts pressure in millibars to altitude in feet
+// https://en.wikipedia.org/wiki/Pressure_altitude
+function altitudeFromPressure(pressure) {
+  return (1 - (pressure/1013.25)**0.190284)*145366.45;
+}
